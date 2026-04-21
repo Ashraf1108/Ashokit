37f7d882d4d422d1e0623cf2baacca278a56983e
ashraf <ashrafmohammed1108@gmail.com>
2026-04-21 00:33:43 -0400

    pandas_04_21

--- pandas.py
+++ pandas.py
@@ -34,7 +34,29 @@ df= pd.DataFrame(data)
 #print(df[df['marks']>50])
 #print(df[df['marks']==50])
 # print(df[df['marks']>50] & (df['marks']<90))
+#print(df.sort_values("marks"))
+#print(df.sort_values("marks", ascending=False))
+#print(df.sort_index())
 
-#example -3
+#df = pd.read_csv("file_name")
+#print(df)
+#print(df["name"])
+#print(df["name","age"])
+
+# print(df.isnull()) # whichever null will show true
+# print(df.notnull()) # whichever not null will show true
+# print(df.dropna()) #remove the row which contains null 
+#print(df.dropna(axis=1)) # remove the column with null
+#print(df.fillna(10)) #add 10 to the element which is null
+# print(df.ffill()) # it check for null and replaces wiyh previous number from top
+# print(df.bfill()) # it checks for null and replaces with previous number from bottom
+
+# print(df["age"].fillna(df["age"].mean())) #for adding missing values with mean 
+
+
+
+
+
+
 
 
